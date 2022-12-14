13603ef811cac736a3239a93e1daa49cb0a37693
givoontake <givoontake@gmail.com>
2022-12-14 21:36:28 +0900

    .

--- objMain.cpp
+++ objMain.cpp
@@ -10,18 +10,22 @@
 #define RED 100
 #define BLUE 101
 
+#include <windows.h>
+#include <mmsystem.h>
 #include "shader.h"
 #include "objRead.h"
+
+#pragma comment(lib,"winmm.lib")
 
 using namespace std;
 
 GLuint g_window_w = 1200;
 GLuint g_window_h = 1000;
 
-GLuint VAO[6];
-GLuint VBO_position[6];
-GLuint VBO_normal[6];
-GLuint VBO_color[6];
+GLuint VAO[8];
+GLuint VBO_position[8];
+GLuint VBO_normal[8];
+GLuint VBO_color[8];
 
 GLuint Chess_Board_vao;
 GLuint box_vao;
@@ -48,18 +52,21 @@ void ambientlight(float R, float G, float B);
 void Chess_Board_ambientlight();
 void Chess_obj_draw();
 void init_Chess_board();
+void back_turn();
+void reset();
 void WhiteTurn(unsigned char key);
 void BlackTurn(unsigned char key);
-void TimerFunction(int);
+void TimerFunction(int value);
 
 struct Chess {
 	float x, z;
-	int p; //piece
-	int c; // color
+	int piece; //piece
+	int color; // color
 };
 
 struct Chess board[8][8];
 struct Chess selected;
+struct Chess backup[8][8];
 
 float Chess_Board_location[] = {
 	0.0f, 0.0f, 0.0f,				// A-1
@@ -1074,13 +1081,16 @@ bool RedCam = false;
 bool BlueCam = false;
 
 //--- load obj related variabales
-objRead objReader[6];
+objRead objReader[8];
 GLint Pawn = objReader[0].loadObj_normalize_center("Pawn.obj");
 GLint Knight = objReader[1].loadObj_normalize_center("Knight.obj");
 GLint Bishop = objReader[2].loadObj_normalize_center("BIshop.obj");
 GLint Rook = objReader[3].loadObj_normalize_center("Rook.obj");
 GLint Queen = objReader[4].loadObj_normalize_center("Queen.obj");
 GLint King = objReader[5].loadObj_normalize_center("King.obj");
+GLint Chair = objReader[6].loadObj_normalize_center("Chair.obj");
+GLint Table = objReader[7].loadObj_normalize_center("Table.obj");
+
 
 int main(int argc, char** argv)
 {
@@ -1145,15 +1155,15 @@ int main(int argc, char** argv)
 
 void InitBuffer()
 {
-	glGenVertexArrays(6, VAO);
-	glGenBuffers(6, VBO_position);
-	glGenBuffers(6, VBO_normal);
-	glGenBuffers(6, VBO_color);
+	glGenVertexArrays(8, VAO);
+	glGenBuffers(8, VBO_position);
+	glGenBuffers(8, VBO_normal);
+	glGenBuffers(8, VBO_color);
 
 	GLint cAttribute = glGetAttribLocation(s_program[0], "aColor");
 	GLint pAttribute = glGetAttribLocation(s_program[0], "aPos");
 	GLint nAttribute = glGetAttribLocation(s_program[0], "aNormal");
-	for (int i = 0; i < 6; i++)
+	for (int i = 0; i < 8; i++)
 	{
 		glUseProgram(s_program[0]);
 		glBindVertexArray(VAO[i]);
@@ -1223,11 +1233,23 @@ void Display()
 	//*************************************************************************
 	// Drawing circle
 	glUseProgram(s_program[0]);
-	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 	transform(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0);
 	viewTransform();
 	projectionTransform();
+	ambientlight(1.0, 0.5, 0.0);
+	glBindVertexArray(VAO[6]);
+	transform(14.0, 0.0, 4.0, 0.0, 0.0, 0.0, 4.0, 4.0, 4.0);
+	glDrawArrays(GL_TRIANGLES, 0, Chair);
+	transform(-6.0, 0.0, 4.0, 0.0, 180.0, 0.0, 4.0, 4.0, 4.0);
+	glDrawArrays(GL_TRIANGLES, 0, Chair);
+	glBindVertexArray(VAO[7]);
+	transform(4.0, -5.01, 4.0, 0.0, 0.0, 0.0, 5.0, 5.0, 5.0);
+	glDrawArrays(GL_TRIANGLES, 0, Table);
+
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 	ambientlight(1.0, 1.0, 1.0);
+	transform(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0);
 	glBindVertexArray(Chess_Board_vao);
 	glDrawArrays(GL_QUADS, 0, 32 * 8);
 	glBindVertexArray(box_vao);
@@ -1258,48 +1280,83 @@ void Keyboard(unsigned char key, int x, int y)
 {
 	switch (key)
 	{
-	case '[':
+	case 32:
+		for (int i = 0; i < 8; i++) // 무르기용 -> 한 수를 두고 다음 말을 선택하기 전에는 그 전으로 무를 수 있게
+		{
+			for (int j = 0; j < 8; j++)
+			{
+				backup[i][j].piece = board[i][j].piece;
+				backup[i][j].color = board[i][j].color;
+			}
+		}
 		if (turn == RED) {
-			if (board[box_i][box_j].p != NONE && board[box_i][box_j].c == RED) {
+			if (board[box_i][box_j].piece != NONE && board[box_i][box_j].color == RED) {
 				sel = true;
-				selected.p = board[box_i][box_j].p;
-				selected.c = board[box_i][box_j].c;
-				board[box_i][box_j].p = NONE;
-				board[box_i][box_j].c = NONE;
+				selected.piece = board[box_i][box_j].piece;
+				selected.color = board[box_i][box_j].color;
+				board[box_i][box_j].piece = NONE;
+				board[box_i][box_j].color = NONE;
 				temp_i = box_i;
 				temp_j = box_j;
 			}
-			break;
 		}
 		else if (turn == BLUE) {
-			if (board[box_i][box_j].p != NONE && board[box_i][box_j].c == BLUE) {
+			if (board[box_i][box_j].piece != NONE && board[box_i][box_j].color == BLUE) {
 				sel = true;
-				selected.p = board[box_i][box_j].p;
-				selected.c = board[box_i][box_j].c;
-				board[box_i][box_j].p = NONE;
-				board[box_i][box_j].c = NONE;
+				selected.piece = board[box_i][box_j].piece;
+				selected.color = board[box_i][box_j].color;
+				board[box_i][box_j].piece = NONE;
+				board[box_i][box_j].color = NONE;
 				temp_i = box_i;
 				temp_j = box_j;
 			}
-			break;
 		}
+		break;
 
-	case ']':
+	case 13:
 		if (sel == true) {
+			switch (board[box_i][box_j].piece) {
+			case PAWN:
+				PlaySound(TEXT("Pawn_die.wav"), 0, SND_FILENAME | SND_ASYNC);
+				break;
+
+			case KNIGHT:
+				PlaySound(TEXT("Knight_die.wav"), 0, SND_FILENAME | SND_ASYNC);
+				break;
+
+			case BISHOP:
+				PlaySound(TEXT("Bishop_die.wav"), 0, SND_FILENAME | SND_ASYNC);
+				break;
+
+			case ROOK:
+				PlaySound(TEXT("Rook_die.wav"), 0, SND_FILENAME | SND_ASYNC);
+				break;
+
+			case QUEEN:
+				PlaySound(TEXT("Queen_die.wav"), 0, SND_FILENAME | SND_ASYNC);
+				break;
+
+			case KING:
+				PlaySound(TEXT("King_die.wav"), 0, SND_FILENAME | SND_ASYNC);
+				break;
+
+			case NONE:
+				break;
+			}
 			if (turn == RED) {
-				if (board[box_i][box_j].c != RED) {
+				if (board[box_i][box_j].color != RED) {
 					sel = false;
-					board[box_i][box_j].p = selected.p;
-					board[box_i][box_j].c = selected.c;
+					board[box_i][box_j].piece = selected.piece;
+					board[box_i][box_j].color = selected.color;
 					turn = BLUE;
 					BlueCam = true;
 				}
 			}
 			else if (turn == BLUE) {
-				if (board[box_i][box_j].c != BLUE) {
+				if (board[box_i][box_j].color != BLUE) {
 					sel = false;
-					board[box_i][box_j].p = selected.p;
-					board[box_i][box_j].c = selected.c;
+					board[box_i][box_j].piece = selected.piece;
+					board[box_i][box_j].color = selected.color;
 					turn = RED;
 					RedCam = true;
 				}
@@ -1315,11 +1372,29 @@ void Keyboard(unsigned char key, int x, int y)
 	case 'z':
 		dis += 1.0f;
 		break;
-	case'x':
+	case 'x':
 		shake += 0.5f;
 		break;
 	case 'c':
 		pourout += 1.0f;
+		break;
+	case 'b':
+		back_turn();
+		break;
+	case 't':
+		if (turn == RED) {
+			turn = BLUE;
+			BlueCam = true;
+		}
+
+		else {
+			turn = RED;
+			RedCam = true;
+		}
+		break;
+
+	case 'r':
+		reset();
 		break;
 	}
 	glutPostRedisplay();
@@ -1396,34 +1471,34 @@ void init_Chess_board() {
 			board[i][j].x = 0.5 + 1.0 * i;
 			board[i][j].z = 0.5 + 1.0 * j;
 			if (i == 1 or i == 6)
-				board[i][j].p = PAWN;
+				board[i][j].piece = PAWN;
 			else
-				board[i][j].p = NONE;
+				board[i][j].piece = NONE;
 			if (i < 2)
-				board[i][j].c = RED;
+				board[i][j].color = RED;
 			else if (i > 5)
-				board[i][j].c = BLUE;
+				board[i][j].color = BLUE;
 			else
-				board[i][j].c = NONE;
+				board[i][j].color = NONE;
 		}
 	}
-	board[0][0].p = ROOK;
-	board[0][1].p = KNIGHT;
-	board[0][2].p = BISHOP;
-	board[0][3].p = QUEEN;
-	board[0][4].p = KING;
-	board[0][5].p = BISHOP;
-	board[0][6].p = KNIGHT;
-	board[0][7].p = ROOK;
+	board[0][0].piece = ROOK;
+	board[0][1].piece = KNIGHT;
+	board[0][2].piece = BISHOP;
+	board[0][3].piece = QUEEN;
+	board[0][4].piece = KING;
+	board[0][5].piece = BISHOP;
+	board[0][6].piece = KNIGHT;
+	board[0][7].piece = ROOK;
 
-	board[7][0].p = ROOK;
-	board[7][1].p = KNIGHT;
-	board[7][2].p = BISHOP;
-	board[7][3].p = KING;
-	board[7][4].p = QUEEN;
-	board[7][5].p = BISHOP;
-	board[7][6].p = KNIGHT;
-	board[7][7].p = ROOK;
+	board[7][0].piece = ROOK;
+	board[7][1].piece = KNIGHT;
+	board[7][2].piece = BISHOP;
+	board[7][3].piece = KING;
+	board[7][4].piece = QUEEN;
+	board[7][5].piece = BISHOP;
+	board[7][6].piece = KNIGHT;
+	board[7][7].piece = ROOK;
 
 }
 
@@ -1433,15 +1508,18 @@ void Chess_obj_draw()
 	viewTransform();
 	projectionTransform();
 
+	int king = 0;
 	for (int i = 0; i < 8; i++)
 	{
 		for (int j = 0; j < 8; j++)
 		{
-			if (board[i][j].c == RED)
+			if (board[i][j].color == RED)
 				ambientlight(1.0, 0.0, 0.0);
-			else if (board[i][j].c == BLUE)
+			else if (board[i][j].color == BLUE)
 				ambientlight(0.0, 0.0, 1.0);
-			switch (board[i][j].p)
+			if (board[i][j].piece == KING)
+				king++;
+			switch (board[i][j].piece)
 			{
 			case PAWN:
 				glBindVertexArray(VAO[0]);
@@ -1482,11 +1560,11 @@ void Chess_obj_draw()
 		}
 	}
 	if (sel == true) {
-		if (selected.c == RED)
+		if (selected.color == RED)
 			ambientlight(1.0, 0.0, 0.0);
-		else if (selected.c == BLUE)
+		else if (selected.color == BLUE)
 			ambientlight(0.0, 0.0, 1.0);
-		switch (selected.p)
+		switch (selected.piece)
 		{
 		case PAWN:
 			glBindVertexArray(VAO[0]);
@@ -1525,9 +1603,77 @@ void Chess_obj_draw()
 			break;
 		}
 	}
+	if (king != 2)
+		reset();
 }
 //tz에다가 sin 더하면 양옆으로 흔들기
-//
+
+void back_turn() {
+	for (int i = 0; i < 8; i++)
+	{
+		for (int j = 0; j < 8; j++)
+		{
+			board[i][j].piece = backup[i][j].piece;
+			board[i][j].color = backup[i][j].color;
+		}
+	}
+	if (turn == RED) {
+		turn = BLUE;
+		BlueCam = true;
+	}
+		
+	else {
+		turn = RED;
+		RedCam = true;
+	}
+		
+	sel = false;
+}
+
+void reset() {
+	for (int i = 0; i < 8; i++)
+	{
+		for (int j = 0; j < 8; j++)
+		{
+			board[i][j].x = 0.5 + 1.0 * i;
+			board[i][j].z = 0.5 + 1.0 * j;
+			if (i == 1 or i == 6)
+				board[i][j].piece = PAWN;
+			else
+				board[i][j].piece = NONE;
+			if (i < 2)
+				board[i][j].color = RED;
+			else if (i > 5)
+				board[i][j].color = BLUE;
+			else
+				board[i][j].color = NONE;			
+		}
+	}
+	board[0][0].piece = ROOK;
+	board[0][1].piece = KNIGHT;
+	board[0][2].piece = BISHOP;
+	board[0][3].piece = QUEEN;
+	board[0][4].piece = KING;
+	board[0][5].piece = BISHOP;
+	board[0][6].piece = KNIGHT;
+	board[0][7].piece = ROOK;
+
+	board[7][0].piece = ROOK;
+	board[7][1].piece = KNIGHT;
+	board[7][2].piece = BISHOP;
+	board[7][3].piece = KING;
+	board[7][4].piece = QUEEN;
+	board[7][5].piece = BISHOP;
+	board[7][6].piece = KNIGHT;
+	board[7][7].piece = ROOK;
+
+	t_box_x = 0.0f;
+	t_box_z = 0.0f;
+	box_i = 0;
+	box_j = 0;
+	sel = false;
+	turn = RED;
+}
 
 void transform(float tx, float ty, float tz, float rx, float ry, float rz, float sx, float sy, float sz) {
 	float sin = glm::sin(shake);
@@ -1659,11 +1805,11 @@ void WhiteTurn(unsigned char key) {
 	float cos = glm::cos(angle);
 	switch (key) {
 	case '[':
-		if (board[box_i][box_j].c == RED) {
-			if (board[box_i][box_j].p != NONE) {
+		if (board[box_i][box_j].color == RED) {
+			if (board[box_i][box_j].piece != NONE) {
 				sel = true;
-				selected.p = board[box_i][box_j].p;
-				selected.c = board[box_i][box_j].c;
+				selected.piece = board[box_i][box_j].piece;
+				selected.color = board[box_i][box_j].color;
 				temp_i = box_i;
 				temp_j = box_j;
 			}
@@ -1672,10 +1818,10 @@ void WhiteTurn(unsigned char key) {
 	case ']':
 		if (sel == true) {
 			sel = false;
-			board[box_i][box_j].p = selected.p;
-			board[box_i][box_j].c = selected.c;
-			board[temp_i][temp_j].p = NONE;
-			board[temp_i][temp_j].c = NONE;
+			board[box_i][box_j].piece = selected.piece;
+			board[box_i][box_j].color = selected.color;
+			board[temp_i][temp_j].piece = NONE;
+			board[temp_i][temp_j].color = NONE;
 			Chess_Turn += 1;
 			BlueCam = true;
 		}
@@ -1704,11 +1850,11 @@ void BlackTurn(unsigned char key) {
 	int temp_j = -1;
 	switch (key) {
 	case '[':
-		if (board[box_i][box_j].c == BLUE) {
-			if (board[box_i][box_j].p != NONE) {
+		if (board[box_i][box_j].color == BLUE) {
+			if (board[box_i][box_j].piece != NONE) {
 				sel = true;
-				selected.p = board[box_i][box_j].p;
-				selected.c = board[box_i][box_j].c;
+				selected.piece = board[box_i][box_j].piece;
+				selected.color = board[box_i][box_j].color;
 				temp_i = box_i;
 				temp_j = box_j;
 			}
@@ -1718,10 +1864,10 @@ void BlackTurn(unsigned char key) {
 	case ']':
 		if (sel == true) {
 			sel = false;
-			board[box_i][box_j].p = selected.p;
-			board[box_i][box_j].c = selected.c;
-			board[temp_i][temp_j].p = NONE;
-			board[temp_i][temp_j].c = NONE;
+			board[box_i][box_j].piece = selected.piece;
+			board[box_i][box_j].color = selected.color;
+			board[temp_i][temp_j].piece = NONE;
+			board[temp_i][temp_j].color = NONE;
 			Chess_Turn += 1;
 			RedCam = true;
 		}
